8cbeaa2549d1a4be4f8e1c878f5f35d58cdcff9d
stephengeorge568 <70499850+stephengeorge568@users.noreply.github.com>
2022-08-18 15:51:32 -0500

    Update user_stats float datatype and typo fix

--- aimster-sql/setup/user_stats.sql
+++ aimster-sql/setup/user_stats.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS aimster.aimster_collection (
+CREATE TABLE IF NOT EXISTS aimster.user_stats (
 	id INT NOT NULL AUTO_INCREMENT,
 	fk_user_id INT NOT NULL,
-    accuracy FLOAT(2) NOT NULL DEFAULT 0.00, /* ??? */
+    accuracy FLOAT(2,2) NOT NULL DEFAULT 0.00, /* ??? */
     games_played INT NOT NULL DEFAULT 0,
-    high_score FLOAT(2) NOT NULL DEFAULT 0.00, /* ??? */
+    high_score FLOAT(2,2) NOT NULL DEFAULT 0.00, /* ??? */
     PRIMARY KEY ( id ),
     FOREIGN KEY (fk_user_id) REFERENCES user(id)
 );
